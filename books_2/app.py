8ab1b06ad0e7e9ae2dc59acd7ca05532175b8b15
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-17 20:31:03 -0300

    Chore: add specific status code for routers

--- books_2/app.py
+++ books_2/app.py
@@ -4,38 +4,39 @@ from data import books
 from fastapi import FastAPI, Path, Query
 from models import Book, BookRequest
 from utils import increment_id
+from starlette import status
 
 app = FastAPI()
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return books
 
 
-@app.get("/book/{book_id}")
+@app.get("/book/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book_by_id(book_id: int = Path(gt=0)):
     return [b for b in books if b.id == book_id][0]
 
 
-@app.get("/book/rating/")
+@app.get("/book/rating/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(rating: int = Query(gt=0, lt=6)):
     return [b for b in books if b.rating == rating]
 
 
-@app.get("/book/published-date/")
+@app.get("/book/published-date/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(published_date: int = Query(lt=date.today().year + 1)):
     return [b for b in books if b.published_date == published_date]
 
 
-@app.post("/book")
+@app.post("/book", status_code=status.HTTP_201_CREATED)
 async def insert_new_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     new_book.id = increment_id()
     books.append(new_book)
 
 
-@app.put("/book")
+@app.put("/book", status_code=status.HTTP_205_RESET_CONTENT)
 async def update_book(book_request: BookRequest):
     updated_book = Book(**book_request.model_dump())
     for i in range(len(books)):
@@ -44,7 +45,7 @@ async def update_book(book_request: BookRequest):
             break
 
 
-@app.delete("/book/{book_id}")
+@app.delete("/book/{book_id}", status_code=status.HTTP_205_RESET_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
     for b in books:
         if b.id == book_id:
