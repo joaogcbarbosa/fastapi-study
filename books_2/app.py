41a2a956e8a69aa917a11655ced2773fe68f862d
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-17 21:02:49 -0300

    Chore: add HTTPException

--- books_2/app.py
+++ books_2/app.py
@@ -1,32 +1,43 @@
 from datetime import date
 
 from data import books
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query
 from models import Book, BookRequest
-from utils import increment_id
 from starlette import status
+from utils import increment_id
 
 app = FastAPI()
 
 
 @app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
-    return books
+    if len(books) != 0:
+        return books
+    raise HTTPException(status_code=404, detail="No data was found.")
 
 
 @app.get("/book/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book_by_id(book_id: int = Path(gt=0)):
-    return [b for b in books if b.id == book_id][0]
+    for b in books:
+        if b.id == book_id:
+            return b
+    raise HTTPException(status_code=404, detail="Item not found.")
 
 
 @app.get("/book/rating/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(rating: int = Query(gt=0, lt=6)):
-    return [b for b in books if b.rating == rating]
+    for b in books:
+        if b.rating == rating:
+            return b
+    raise HTTPException(status_code=404, detail="Item not found.")
 
 
 @app.get("/book/published-date/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(published_date: int = Query(lt=date.today().year + 1)):
-    return [b for b in books if b.published_date == published_date]
+    for b in books:
+        if b.published_date == published_date:
+            return b
+    raise HTTPException(status_code=404, detail="Item not found.")
 
 
 @app.post("/book", status_code=status.HTTP_201_CREATED)
@@ -39,15 +50,23 @@ async def insert_new_book(book_request: BookRequest):
 @app.put("/book", status_code=status.HTTP_205_RESET_CONTENT)
 async def update_book(book_request: BookRequest):
     updated_book = Book(**book_request.model_dump())
+    book_changed = False
     for i in range(len(books)):
         if books[i].id == updated_book.id:
             books[i] = updated_book
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found.")
 
 
 @app.delete("/book/{book_id}", status_code=status.HTTP_205_RESET_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
+    book_found = False
     for b in books:
         if b.id == book_id:
             books.remove(b)
+            book_found = True
             break
+    if not book_found:
+        raise HTTPException(status_code=404, detail="Item not found.")
