9c61a6dbbc1933690eb868ad40c30b5ebd692765
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-18 23:08:03 -0300

    Chore: different logic to get db session

--- todo/config.py
+++ todo/config.py
@@ -1,15 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from contextlib import contextmanager
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db" 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_session() -> Session:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    session = Session(bind=engine)
+    return session
