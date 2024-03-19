58b6c6792e6d2a34dda05fa67e21ef0d07a98914
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-18 23:06:14 -0300

    Chore: get and post routers

--- todo/app.py
+++ todo/app.py
@@ -1,2 +1,34 @@
+from fastapi import FastAPI, HTTPException, Path
+from config import get_session
+from starlette import status
+from models.models_db import Todo
+from models.models_request import TodoRequest
 
 
+app = FastAPI()
+
+
+@app.get("/todos", status_code=status.HTTP_200_OK)
+async def get_all_todos():
+    db = get_session()
+    todos = db.query(Todo).all()
+    if todos is None:
+        raise HTTPException(status_code=404, detail="No data was found.")
+    return todos
+
+
+@app.get("/todo{todo_id}", status_code=status.HTTP_200_OK)
+async def get_all_todos(todo_id: int = Path(gt=0)):
+    db = get_session()
+    todo = db.query(Todo).filter_by(id=todo_id).first()
+    if todo is None:
+        raise HTTPException(status_code=404, detail="Item not found.")
+    return todo
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def insert_todo(todo_request: TodoRequest):
+    new_todo = Todo(**todo_request.model_dump())
+    db = get_session()
+    db.add(new_todo)
+    db.commit()
