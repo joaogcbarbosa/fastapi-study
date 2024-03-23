3564a171ef69ac75459d171cb9ccab64a739b30c
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-23 14:48:45 -0300

    Chore: renamed

--- todo/create_db.py
+++
@@ -1,8 +0,0 @@
-from config import DBConnection
-from sqlalchemy.orm import declarative_base
-
-db_conn = DBConnection()
-engine = db_conn.get_engine()
-
-Base = declarative_base()
-Base.metadata.create_all(bind=engine)
