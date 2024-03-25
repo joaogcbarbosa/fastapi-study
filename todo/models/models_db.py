1f24849e34c427802acf5b9718ec6e072fe3abe8
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-25 00:08:59 -0300

    Chore: class users

--- todo/models/models_db.py
+++ todo/models/models_db.py
@@ -8,6 +8,18 @@ class Base(DeclarativeBase):
     pass
 
 
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    role: Mapped[str] = mapped_column(String(20), nullable=False)
+
+
 class Todo(Base):
     __tablename__ = "todos"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
