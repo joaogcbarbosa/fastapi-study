e1713eef6d1a209e821f62e4b5e809b091e7f40c
joaogcbarbosa <joaogcb29@gmail.com>
2024-03-31 23:15:15 -0300

    Feat: function to authenticate user and create access token

--- todo/routers/auth.py
+++ todo/routers/auth.py
@@ -1,24 +1,50 @@
-from fastapi import APIRouter
-from starlette import status
-from models.models_request import UserRequest
-from models.models_db import User
+from typing import Annotated
+
 from config import DBConnection
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from models.models_db import User
+from models.models_request import UserRequest, Token
+from passlib.context import CryptContext
+from starlette import status
+from utils.authentication import authenticate_user, create_access_token
+from datetime import timedelta
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
 db_conn = DBConnection()
+bcrypt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
-async def insert_user(user_request: UserRequest):
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_user(user_request: UserRequest):
     new_user = User(
         email=user_request.email,
         username=user_request.username,
         first_name=user_request.first_name,
         last_name=user_request.last_name,
-        hashed_password=user_request.password,
+        hashed_password=bcrypt.hash(user_request.password),
         is_active=True,
-        role=user_request.role
+        role=user_request.role,
     )
     session = db_conn.get_session()
     session.add(new_user)
     session.commit()
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user."
+        )
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+    }
