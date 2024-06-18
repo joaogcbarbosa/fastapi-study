0dc41b23ee623c574258a4068c4a6473449e0165
joaogcbarbosa <joao.barbosa@rioenergy.com.br>
2024-06-18 00:25:15 -0300

    Chore: add dunder main

--- todo/app.py
+++ todo/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from routers import auth, todos
+import uvicorn
 
 
-app = FastAPI()
+app = FastAPI(debug=True)
 app.include_router(auth.router)
 app.include_router(todos.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, port=3000)
