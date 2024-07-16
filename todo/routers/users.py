a5adcdbfae7e029da522ccaab8653c18494ab74a
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-16 20:52:49 -0300

    Chore: router to update phone number

--- todo/routers/users.py
+++ todo/routers/users.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from config import DBConnection
 from fastapi import APIRouter, Depends, HTTPException
 from models.models_db import User
-from models.models_request import UserVerification
+from models.models_request import UserVerification, UserRequest
 from passlib.context import CryptContext
 from starlette import status
 from utils.authentication import get_current_user
@@ -32,4 +32,13 @@ async def change_password(user: user_dependency, session: db_dependency, user_ve
     if not bcrypt.verify(user_verification.password, actual_user.hashed_password):
         raise HTTPException(status_code=401, detail="Error on password change.")
     actual_user.hashed_password = bcrypt.hash(user_verification.new_password)
+    session.commit()
+
+
+@router.put("", status_code=status.HTTP_204_NO_CONTENT)
+async def update_phone_number(user: user_dependency, session: db_dependency, user_request: UserRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed.")
+    actual_user = session.query(User).filter_by(id=user["id"]).first()
+    actual_user.phone_number = user_request.phone_number
     session.commit()
