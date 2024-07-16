7d9f8d92d476429b4ceac78519533f4ed585def4
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-16 18:39:33 -0300

    Chore: adds router

--- todo/app.py
+++ todo/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import auth, todos
+from routers import auth, todos, users
 import uvicorn
 
 
 app = FastAPI(debug=True)
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(users.router)
 
 if __name__ == "__main__":
     uvicorn.run(app=app, port=3000)
