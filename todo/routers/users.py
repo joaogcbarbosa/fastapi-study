cfbda1560ff3ad2a6f5e9c7b4a2016f13a868142
joaogcbarbosa <joaogcb29@gmail.com>
2024-07-16 19:21:04 -0300

    Chore: checking if password matches

--- todo/routers/users.py
+++ todo/routers/users.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from config import DBConnection
 from fastapi import APIRouter, Depends, HTTPException
 from models.models_db import User
-from models.models_request import UserRequest
+from models.models_request import UserVerification
 from passlib.context import CryptContext
 from starlette import status
 from utils.authentication import get_current_user
@@ -25,9 +25,11 @@ async def get_user(user: user_dependency, session: db_dependency):
 
 
 @router.put("", status_code=status.HTTP_204_NO_CONTENT)
-async def change_password(user: user_dependency, session: db_dependency, user_request: UserRequest):
+async def change_password(user: user_dependency, session: db_dependency, user_verification: UserVerification):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication Failed.")
     actual_user = session.query(User).filter_by(id=user["id"]).first()
-    actual_user.hashed_password = bcrypt.hash(user_request.password)
+    if not bcrypt.verify(user_verification.password, actual_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Error on password change.")
+    actual_user.hashed_password = bcrypt.hash(user_verification.new_password)
     session.commit()
