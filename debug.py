f48a2e7f0efed1ee38fb49e6bd4689dac6718394
joaogcbarbosa <joaogcb29@gmail.com>
2024-09-03 19:35:31 -0300

    Chore: alter path

--- debug.py
+++ debug.py
@@ -3,11 +3,11 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        "todo.main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=3000,
         reload=True,
         headers=[],
-        reload_dirs=["todo"],
+        reload_dirs=["app"],
         reload_delay=2,
     )
