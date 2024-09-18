ff4672e80147e78156a99379e6b31c57f2c5b919
joaogcbarbosa <joaogcb29@gmail.com>
2024-09-17 22:28:40 -0300

    Chore: fixtures

--- tests/fixtures/test_client.py
+++ tests/fixtures/test_client.py
@@ -1,20 +1,22 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
-from app.main import app
-from sqlalchemy.pool import StaticPool
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
-from app.init_db import Base
-from app.config.db import DBConnection
+from sqlalchemy.pool import StaticPool
 
+from app.config.db import DBConnection
+from app.init_db import Base
+from app.main import app
 
 load_dotenv()
 
-MSSQL_USER=os.getenv("MSSQL_USER")
-MSSQL_PASSWD=os.getenv("MSSQL_PASSWD")
-MSSQL_HOST=os.getenv("MSSQL_HOST")
-MSSQL_PORT=os.getenv("MSSQL_PORT")
+MSSQL_USER = os.getenv("MSSQL_USER")
+MSSQL_PASSWD = os.getenv("MSSQL_PASSWD")
+MSSQL_HOST = os.getenv("MSSQL_HOST")
+MSSQL_PORT = os.getenv("MSSQL_PORT")
 TEST_DB_URL = f"mssql+pymssql://{MSSQL_USER}:{MSSQL_PASSWD}@{MSSQL_HOST}:{MSSQL_PORT}/todos"
 
 
@@ -24,17 +26,29 @@ test_engine = create_engine(
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
 
-
 Base.metadata.create_all(bind=test_engine)
 
 
-def override_get_session():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
+@pytest.fixture(scope="function")
+def db_session():
+    connection = test_engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
 
-app.dependency_overrides[DBConnection().get_session] = override_get_session
 
-test_client = TestClient(app=app, base_url="http://0.0.0.0:3000")
+@pytest.fixture(scope="function")
+def test_client(db_session):
+
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            db_session.close()
+
+    app.dependency_overrides[DBConnection().get_session] = override_get_db
+    with TestClient(app) as test_client:
+        yield test_client
