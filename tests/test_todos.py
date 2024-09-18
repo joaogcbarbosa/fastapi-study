ce6c217e63337fabcc5c1ec21cd36ec2e1ec5aa9
joaogcbarbosa <joaogcb29@gmail.com>
2024-09-17 22:28:54 -0300

    Chore: getting test client fixture

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,7 +1,7 @@
 from .fixtures.test_client import test_client
 
 
-def test_get_all():
+def test_get_all(test_client):
     response = test_client.get("/todo/todos")
     print(response)
     assert response.status_code == 200
