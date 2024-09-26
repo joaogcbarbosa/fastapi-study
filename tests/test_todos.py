79f891747d2fc20ac3dee6e0e9dacd92890197ac
joaogcbarbosa <joaogcb29@gmail.com>
2024-09-25 22:45:00 -0300

    Chore: testing test_user's login

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -1,4 +1,12 @@
-from .fixtures.test_client import test_client, db_session
+from .fixtures import test_client, db_session, test_user
+
+
+def test_login_for_access_token(test_client, test_user):
+  response = test_client.post("/auth/token", data=test_user)
+  assert response.status_code == 200
+  token = response.json()["access_token"]
+  assert token is not None
+  return token
 
 
 def test_get_all(test_client):
